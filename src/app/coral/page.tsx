a848a4786f6595a032324ec3e1b704bda54254c2
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-14 19:24:06 +0100

    Add /coral short route

---
+++ src/app/coral/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=QXKVjBoYvpuy8EHORYLaY5MaFPJOuSZuf_c-dy6lyC4&m=%20El%20abuelo%20siempre%20nos%20cantar%C3%A1%20desde%20el%20cielo."
+  );
+}
