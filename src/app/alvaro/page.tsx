60c5e811e96851a1e5055eef1c9aaaf5c81a74cd
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-15 17:35:19 +0100

    Add short link /alvaro

---
+++ src/app/alvaro/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=2YUNHwqU23J64ooGEgX5IWqZmLsz7PMWR8n6mlmTDLA&m=1%20a%C3%B1ito%20de%20Valentina"
+  );
+}
