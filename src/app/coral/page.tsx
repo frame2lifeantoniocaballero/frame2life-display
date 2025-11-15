9e320a3ce9b4e0776c5adf00e93a352721d59c1e
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-15 17:22:45 +0100

    Update coral message

--- src/app/coral/page.tsx
+++ src/app/coral/page.tsx
@@ -2,6 +2,6 @@ import { permanentRedirect } from "next/navigation";
 
 export default function Page() {
   permanentRedirect(
-    "/v?v=QXKVjBoYvpuy8EHORYLaY5MaFPJOuSZuf_c-dy6lyC4&m=%20El%20abuelo%20siempre%20nos%20cantar%C3%A1%20desde%20el%20cielo."
+    "/v?v=QXKVjBoYvpuy8EHORYLaY5MaFPJOuSZuf_c-dy6lyC4&m=El%20abuelo%20Pedro%20hoy%20y%20siempre%20nos%20cantar%C3%A1."
   );
 }
