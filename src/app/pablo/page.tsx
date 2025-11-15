b54a0a97063dad283cc5cec4252d2fcc5405b55e
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-15 17:27:36 +0100

    Add short link /pablo

---
+++ src/app/pablo/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=BclESVZ7hz7e4LVeZOD3OTt0Qjn8Fse_GZFrGxPh5GM&m=Punta%20Cana%202025"
+  );
+}
