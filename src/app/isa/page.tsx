66b67f999ac4238fadc4bd569b3f227b3c00033c
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-15 17:33:33 +0100

    Add short link /isa

---
+++ src/app/isa/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=O3Z8MQ4-RCt-HMtMNReRfcCIBaUgXwuSEh2YOaV_PpQ&m=Mensajes%20del%20pasado%20que%20reflejan%20realidades%20del%20presente%2C%20te%20queremos%20hoy%20y%20siempre.%20Nuestra%20persona%20favorita."
+  );
+}
