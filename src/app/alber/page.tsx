048c071d49f70b5658cf3c7c4b4c5622d2785bcd
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-16 11:12:56 +0100

    Add /alber short link

---
+++ src/app/alber/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=Eh76zyzjjnOz9RkAQPwD7jF4Q1wGiw3MTejfLqZ4X0Y&m=Gracias%20por%20aparecer%20un%20d%C3%ADa%20cualquiera%2C%20y%20cambiar%20mi%20mundo%20para%20siempre."
+  );
+}
