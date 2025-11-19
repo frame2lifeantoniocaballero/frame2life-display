9428836ddaa6a4fb8fc6c49b8bc592c2bb12630e
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-19 12:37:17 +0100

    Add short link /paula

---
+++ src/app/paula/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=-KXmMHLN4QV2r0TxJFrt_fG3B3SwqrVzTOvndRKDDP4&m=Recuerdo%20guardado%20para%20siempre.%20Te%20quiero%2C%20amiga."
+  );
+}
