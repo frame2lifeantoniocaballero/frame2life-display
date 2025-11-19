58a453b40b3141826612b4d06d414b4c16751a9e
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-19 12:32:06 +0100

    Add short link /momentos

---
+++ src/app/miriam/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=mQQKDhF4XTFSvlFENPzSIJj8GOnZ5r3gafbkHtNl9U8&m=Momentos%20para%20no%20olvidar%20nunca"
+  );
+}
