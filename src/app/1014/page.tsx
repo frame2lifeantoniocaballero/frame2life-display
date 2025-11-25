4d42434098f195aa01fc16d44234bb5dacba8220
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-25 11:50:41 +0100

    Add short link /1014

---
+++ src/app/1014/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=gK-ZEQkYSyvfG0qZaZn0OMGRbtVqQ-RJFQcAACXf-Uo&m=Momentos%20en%20familia"
+  );
+}
