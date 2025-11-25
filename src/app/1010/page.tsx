1f5a442b296ac0238798359fcce68d4ae780723f
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-25 12:26:15 +0100

    Add short link /1010

---
+++ src/app/1010/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=ukLNLNKakeCUQYu2UnEX2xmmrEXwkPwtwGaNyBcshe8&m=18-06-2025"
+  );
+}
