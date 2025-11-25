731de3a9c2a951d59bb75c7238ad5185a78ac845
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-11-25 12:22:04 +0100

    Add short link /1016

---
+++ src/app/1016/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=-bo2Lj7JGiyYSmhgD--7-eaHXGQXdttm11njEou-AQw&m=Que%20nunca%20nos%20falten%20momentos%20juntos"
+  );
+}
