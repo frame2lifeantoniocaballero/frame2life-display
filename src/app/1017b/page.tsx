45e82cccd0af3097e0e2ee47240137557b248262
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-12-08 22:16:09 +0100

    Fix dynamic redirect pages for 1017a, 1017b, 1017c

--- src/app/1017b/page.tsx
+++ src/app/1017b/page.tsx
@@ -1,5 +1,7 @@
 import { permanentRedirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   permanentRedirect(
     "/v?v=EkOHXGFggI2qvodmuEHRWOmuobJ-zbMVKROt2IJyVZg&m=Coge%20el%20tel%C3%A9fono!"
