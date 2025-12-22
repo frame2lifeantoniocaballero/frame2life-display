592a2e65eea7bf446a1eb0d6ce856d61fae5c5bf
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-12-22 16:59:01 +0100

    Fix redirect for /2021

---
+++ src/app/2021/page.tsx
@@ -0,0 +1,9 @@
+import { permanentRedirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=2XjTTBjpWhhG0FyT_v2XR0B9-gPC2-SrWPUgIcE-iZc&m=Dedicada%20a%20mi%20abuela"
+  );
+}
