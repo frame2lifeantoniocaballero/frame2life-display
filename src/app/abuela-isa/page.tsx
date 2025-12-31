db13544705d0bbb35b98aa8a4de3db2fac49f0dd
frame2life2antoniocaballero <frame2life2@gmail.com>
2025-12-31 11:09:24 +0100

    Add short link /abuela-isa

---
+++ src/app/abuela-isa/page.tsx
@@ -0,0 +1,9 @@
+import { permanentRedirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=ZEnW66AE8YONmbYGIzil95TTis8nvOXQXeDD2CYNRDQ&m=Los%20pi%C3%B1ones%20siempre%20juntos"
+  );
+}
