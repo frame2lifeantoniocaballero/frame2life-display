c1d53d83dfa1c3c17e53d9f89105f343483d38ed
frame2life2antoniocaballero <frame2life2@gmail.com>
2026-01-02 08:06:10 +0100

    Add short link /1022

---
+++ src/app/1022/page.tsx
@@ -0,0 +1,9 @@
+import { permanentRedirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=HCNn6wynTkY8J9NDSeWvi0kVIX4tNf3PUqFTUqnqoOo&m=Los%20recuerdos%20que%20creamos%20juntos%20son%20nuestro%20tesoro%20m%C3%A1s%20bonito"
+  );
+}
