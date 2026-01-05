e3a30b57cf69b9e3f6d07c00aabe8b648d8450f8
frame2life2antoniocaballero <frame2life2@gmail.com>
2026-01-05 13:31:29 +0100

    Add short link /luna

---
+++ src/app/luna/page.tsx
@@ -0,0 +1,9 @@
+import { permanentRedirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=xrLp_zYWZZjd5Z0nf0GcFUcYOQ2rjj2FcfF6o6BwVM8&m=Para%20Lucrecia%2C%20de%20todos%20tus%20nietos%20con%20mucho%20amor."
+  );
+}
