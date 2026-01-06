63b5973196a788a6f30e26d0195305c700784851
frame2life2antoniocaballero <frame2life2@gmail.com>
2026-01-06 19:05:31 +0100

    Add short link /administracion

---
+++ src/app/administracion/page.tsx
@@ -0,0 +1,9 @@
+import { permanentRedirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  permanentRedirect(
+    "/v?v=Nc8aV7FNWF7DME_ApfvCibCWZUbXylj49NZS0UMElzA&m=Con%20cari%C3%B1o%20para%20la%20UNAD%20de%20Parque%20Europa."
+  );
+}
